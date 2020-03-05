7edbf77ea88cb36103067bd2e489a97d5c8b4417
Protein Dispenser <60122610+MeidenWeiss@users.noreply.github.com>
2020-03-05 14:14:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
   <h1> API Lists </h1>
   <p>
-    Read All: https://api-packages.herokuapp.com/movie_api/movies/read.php <br>
-    Read One: https://api-packages.herokuapp.com/movie_api/movies/read_one.php?id=2001 <br>
-    Categories: https://api-packages.herokuapp.com/movie_api/category/read.php <br>
-    Update: https://api-packages.herokuapp.com/movie_api/movies/update.php <br>
-    Create: https://api-packages.herokuapp.com/movie_api/movies/create.php <br>
-    Delete: https://api-packages.herokuapp.com/movie_api/movies/delete.php&id="ID HERE" <br>
-    Search: https://api-packages.herokuapp.com/movie_api/movies/search.php?name="Title Movie" <br>
+    Read All: https://api-packages.herokuapp.com/movie_api/movies/read.php <br><br>
+    Read One: https://api-packages.herokuapp.com/movie_api/movies/read_one.php?id=2001 <br><br>
+    Categories: https://api-packages.herokuapp.com/movie_api/category/read.php <br><br>
+    Update: https://api-packages.herokuapp.com/movie_api/movies/update.php <br><br>
+    Create: https://api-packages.herokuapp.com/movie_api/movies/create.php <br><br>
+    Delete: https://api-packages.herokuapp.com/movie_api/movies/delete.php&id="ID HERE" <br><br>
+    Search: https://api-packages.herokuapp.com/movie_api/movies/search.php?name="Title Movie" <br><br>
   </p>
 </html>
